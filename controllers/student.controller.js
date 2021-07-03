ab5c0838d60de65f07fe3ed0a0e009f532301e28
serii <seriiburduja@gmail.com>
2021-07-03 09:27:34 +0300

    validate

--- controllers/student.controller.js
+++ controllers/student.controller.js
@@ -2,6 +2,8 @@ const Sequelize = require("sequelize");
 const Op = Sequelize.Op;
 const Student = require("./../models").Student;
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const jwtConfig = require("./../config/jwt.config");
 
 const create = async (req, res) => {
   const { name, email, password } = req.body;
@@ -30,4 +32,52 @@ const create = async (req, res) => {
   }
 };
 
-module.exports = { create };
+const login = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const student = await Student.findOne({
+      where: {
+        email: {
+          [Op.eq]: email
+        }
+      }
+    });
+    if (student) {
+      const correctPassword = await bcrypt.compare(password, student.password);
+      if (correctPassword) {
+        const token = jwt.sign(
+          { id: student.id, email: student.email },
+          jwtConfig.secret,
+          {
+            expiresIn: 60 * 2
+          }
+        );
+
+        res.json({ message: "Student is logged in", token });
+      } else {
+        res.status(404).json({ message: "Student password doesn't exists" });
+      }
+    } else {
+      res.status(404).json({ message: "Student email doesn't exists" });
+    }
+  } catch (error) {
+    console.log(error, "error");
+  }
+};
+
+const validate = async (req, res) => {
+  const token = req.headers["authorization"];
+  if (token) {
+    jwt.verify(token, jwtConfig.secret, (error, decode) => {
+      if (error) {
+        res.status(500).json(error);
+      } else {
+        res.json(decode);
+      }
+    });
+  } else {
+    res.status(404).json({ message: "Token not found" });
+  }
+};
+
+module.exports = { create, login, validate };
--- routes/student.route.js
+++ routes/student.route.js
@@ -1,7 +1,13 @@
 const { Router } = require("express");
 const router = Router();
-const { create } = require("./../controllers/student.controller");
+const {
+  create,
+  login,
+  validate
+} = require("./../controllers/student.controller");
 
 router.post("/student/create", create);
+router.post("/student/login", login);
+router.post("/student/validate", validate);
 
 module.exports = router;
